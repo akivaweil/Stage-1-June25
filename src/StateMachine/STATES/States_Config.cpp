e7af764e2171fce9f12d8e666edde3520eee7e8c
akivaweil <akivaweil@gmail.com>
2025-09-15 15:07:29 -0700

    Before investigating servo jerky behavior

--- src/StateMachine/STATES/States_Config.cpp
+++ src/StateMachine/STATES/States_Config.cpp
@@ -10,8 +10,8 @@
 //* ************************ SERVO CONFIGURATION **************************
 //* ************************************************************************
 // Rotation servo position settings
-const int ROTATION_SERVO_HOME_POSITION = 23.5;
-const int ROTATION_SERVO_ACTIVE_POSITION = 90;
+const int ROTATION_SERVO_HOME_POSITION = 12.0;
+const int ROTATION_SERVO_ACTIVE_POSITION = 105;
 
 //* ************************************************************************
 //* ************************ MOTOR CONFIGURATION **************************
@@ -19,7 +19,7 @@ const int ROTATION_SERVO_ACTIVE_POSITION = 90;
 // Motor step calculations and travel distances
 const float CUT_MOTOR_STEPS_PER_INCH = 500.0;  // 4x increase from 38
 const float FEED_MOTOR_STEPS_PER_INCH = 1000.0;
-const float CUT_TRAVEL_DISTANCE = 9.1;
+const float CUT_TRAVEL_DISTANCE = 9.2;
 const float FEED_TRAVEL_DISTANCE = 3.4; // 3.4 for 3 inch squares and 3.25 for 2.65 inch squares
 const float CUT_MOTOR_INCREMENTAL_MOVE_INCHES = 0.1;
 const float CUT_MOTOR_MAX_INCREMENTAL_MOVE_INCHES = 0.4;

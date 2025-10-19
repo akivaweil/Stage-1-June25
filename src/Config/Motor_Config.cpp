31213554c8a84484451e712ea5a86da7c9e18f9f
akivaweil <akivaweil@gmail.com>
2025-10-19 15:48:07 -0700

    Fix servo rotation - restore working config values from commit 3a2547f

--- src/Config/Motor_Config.cpp
+++ src/Config/Motor_Config.cpp
@@ -10,8 +10,8 @@
 int ROTATION_SERVO_HOME_POSITION = 27;
 int ROTATION_SERVO_ACTIVE_POSITION = 120;
 
-unsigned long ROTATION_SERVO_ACTIVE_HOLD_DURATION_MS = 50;
-unsigned long ROTATION_SERVO_RETURN_DELAY_MS = 1;
+unsigned long ROTATION_SERVO_ACTIVE_HOLD_DURATION_MS = 2000;
+unsigned long ROTATION_SERVO_RETURN_DELAY_MS = 150;
 unsigned long ROTATION_SERVO_HOME_WAIT_DURATION_MS = 500;
 
 float ROTATION_SERVO_EARLY_ACTIVATION_OFFSET_INCHES = 0.2;
@@ -19,7 +19,7 @@ float ROTATION_SERVO_EARLY_ACTIVATION_OFFSET_INCHES = 0.2;
 //* ************************************************************************
 //* ************************ [🔧] ROTATION CLAMP **************************
 //* ************************************************************************
-unsigned long ROTATION_CLAMP_EXTEND_DURATION_MS = 2100;
+unsigned long ROTATION_CLAMP_EXTEND_DURATION_MS = 2500;
 float ROTATION_CLAMP_EARLY_ACTIVATION_OFFSET_INCHES = 2.7;
 
 //* ************************************************************************
@@ -30,15 +30,15 @@ const int CUT_HOMING_DIRECTION = -1;
 
 // Speeds (defaults - can be modified via dashboard)
 float CUT_MOTOR_NORMAL_SPEED = 640;
-float CUT_MOTOR_NORMAL_ACCELERATION = 16000;
-float CUT_MOTOR_NO_WOOD_SPEED = 512; // 80% of normal speed
-float CUT_MOTOR_RETURN_SPEED = 20000;
+float CUT_MOTOR_NORMAL_ACCELERATION = 25000;
+float CUT_MOTOR_NO_WOOD_SPEED = CUT_MOTOR_NORMAL_SPEED * 0.6;
+float CUT_MOTOR_RETURN_SPEED = 25000;
 float CUT_MOTOR_HOMING_SPEED = 1500;
 
 // Distances & Movement (defaults - can be modified via dashboard)
 float CUT_MOTOR_INCREMENTAL_MOVE_INCHES = 0.1;
 float CUT_MOTOR_MAX_INCREMENTAL_MOVE_INCHES = 0.4;
-float SUCTION_SENSOR_CHECK_DISTANCE_INCHES = 0.5;
+float SUCTION_SENSOR_CHECK_DISTANCE_INCHES = 0.2;
 
 // Timing
 unsigned long CUT_HOME_TIMEOUT = 5000;
@@ -55,8 +55,8 @@ const float FEED_MOTOR_OFFSET_FROM_SENSOR = 0.5;
 // Speeds (defaults - can be modified via dashboard)
 float FEED_MOTOR_NORMAL_SPEED = 22000;
 float FEED_MOTOR_NORMAL_ACCELERATION = 22000;
-float FEED_MOTOR_RETURN_SPEED = 40000;
-float FEED_MOTOR_RETURN_ACCELERATION = 50000;
+float FEED_MOTOR_RETURN_SPEED = 22000;
+float FEED_MOTOR_RETURN_ACCELERATION = 30000;
 float FEED_MOTOR_HOMING_SPEED = 2000;
 
 // Distances & Movement (defaults - can be modified via dashboard)
@@ -65,7 +65,7 @@ float FEED_MOTOR_RETURN_DISTANCE = 0.0;
 //* ************************************************************************
 //* ************************ [🤖] TRANSFER ARM *****************************
 //* ************************************************************************
-unsigned long TA_SIGNAL_DURATION = 1000;
+unsigned long TA_SIGNAL_DURATION = 500;
 float TA_SIGNAL_EARLY_ACTIVATION_OFFSET_INCHES = 0.01;
 
 //* ************************************************************************

0a878e1d097da495295784b1830f53b5f4d995af
akivaweil <akivaweil@gmail.com>
2025-11-17 11:34:30 -0800

    Before making rotation delay change

--- src/Config/config.cpp
+++ src/Config/config.cpp
@@ -30,7 +30,7 @@ int ROTATION_SERVO_ACTIVE_POSITION = 118;                        // Servo active
 unsigned long ROTATION_SERVO_ACTIVE_HOLD_DURATION_MS = 2000;     // Duration to hold active position
 unsigned long ROTATION_SERVO_RETURN_DELAY_MS = 150;              // Delay before returning to home
 unsigned long ROTATION_SERVO_HOME_WAIT_DURATION_MS = 500;        // Wait duration at home position
-unsigned long ROTATION_SERVO_SUCTION_HOLD_DURATION_MS = 200;     // Wait time after suction RELEASED before returning servo home
+unsigned long ROTATION_SERVO_SUCTION_HOLD_DURATION_MS = 200;     // Wait time after suction detected before returning
 float ROTATION_SERVO_ACTIVATION_DISTANCE = 8.2;                  // Servo activation distance from start of cut (inches)
 
 //╔═══╗ ════════════════════════════════════════════════════════ ╔═══╗
